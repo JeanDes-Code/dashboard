3cd87a23f245bfb529978c9761804b32039c2014
JeanDes-Code <78590821+JeanDes-Code@users.noreply.github.com>
2022-09-15 12:28:29 +0200

    ECOMMERCE PAGE : Added Currency Icon

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BsCurrencyDollar } from 'react-icons/bs'
+import { BsCurrencyDollar, BsCurrencyEuro } from 'react-icons/bs'
 import { GoPrimitiveDot } from 'react-icons/go'
 
 import { Stacked, Pie, Button, SparkLine } from './../components'
@@ -10,32 +10,37 @@ const Ecommerce = () => {
     const {} = useStateContext()
 
     return (
-        <div className="mt-12">
-            <div className="flex flex-wrap lg:flex-nowrap justify-center">
+        <div className="mt-24">
+            <div className="flex flex-wrap lg:flex-nowrap justify-center ">
                 <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
                     <div className="flex justify-between items-center">
                         <div>
                             <p className="font-bold text-gray-400">Eearnings</p>
                             <p className="text-2xl">63,448.78€</p>
                         </div>
+                        <button
+                            type="button"
+                            style={{ backgroundColor: 'blue' }}
+                            className="text-2xl opacity-0.9 text-white hover:drop-shadow-xl rounded-full  p-4"
+                        >
+                            <BsCurrencyEuro />
+                        </button>
                     </div>
+
                     <div className="mt-6">
                         <Button
                             color="white"
                             bgColor="blue"
                             text="Download"
                             borderRadius="10px"
-                            size="md"
                         />
                     </div>
                 </div>
-
                 <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
                     {earningData.map((item) => (
                         <div
                             key={item.title}
-                            className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg
-                    md:w-56 p-4 pt-9 rounded-2xl"
+                            className="bg-white h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56  p-4 pt-9 rounded-2xl "
                         >
                             <button
                                 type="button"
@@ -43,7 +48,7 @@ const Ecommerce = () => {
                                     color: item.iconColor,
                                     backgroundColor: item.iconBg,
                                 }}
-                                className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
+                                className="text-2xl opacity-0.9 rounded-full  p-4 hover:drop-shadow-xl"
                             >
                                 {item.icon}
                             </button>
@@ -57,7 +62,7 @@ const Ecommerce = () => {
                                     {item.percentage}
                                 </span>
                             </p>
-                            <p className="text-sm text-gray-400 mt-1">
+                            <p className="text-sm text-gray-400  mt-1">
                                 {item.title}
                             </p>
                         </div>
